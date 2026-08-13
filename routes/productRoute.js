f9d098cdf4851cff112d6388e305738c29ce0bc0
Steven Amin <64430821+Steven-Amin02@users.noreply.github.com>
2026-08-14 01:08:19 +0300

    fix:update product and images are now modified by admin only

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const upload = require('../middlewares/upload');
 const validatorMiddleware = require("../middlewares/validatorMiddleware");
+const { protect, allowedTo } = require('../middlewares/authMiddleware');
 
 const { getAllProducts, createProduct, getProduct, deleteProduct, updateProduct, deleteProductImage, addProductImage } = require('../controllers/productController');
 
@@ -12,13 +13,13 @@ const router = express.Router();
 
 router.route('/')
     .get(getAllProducts)
-    .post(upload.array("images", 4), createProductValidator, validatorMiddleware, createProduct);
+    .post(protect, allowedTo('admin'), upload.array("images", 4), createProductValidator, validatorMiddleware, createProduct);
 router.route('/:id')
     .get(checkIdParamValidator, getProduct)
-    .put(checkIdParamValidator, updateProduct)
-    .delete(checkIdParamValidator, deleteProduct);
+    .put(protect, allowedTo('admin'), checkIdParamValidator, updateProduct)
+    .delete(protect, allowedTo('admin'), checkIdParamValidator, deleteProduct);
 router.route('/:id/images')
-    .post(upload.array("images", 4), checkIdParamValidator, addProductImage)
-    .delete(checkIdParamValidator, deleteProductImage);
+    .post(protect, allowedTo('admin'), upload.array("images", 4), checkIdParamValidator, addProductImage)
+    .delete(protect, allowedTo('admin'), checkIdParamValidator, deleteProductImage);
 
 module.exports = router;
